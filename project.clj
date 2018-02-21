273d61ee666743110361b2197ef30614a273ffab
Wilfred Hughes <me@wilfred.me.uk>
2018-02-21 22:23:50 +0000

    Update versions

--- project.clj
+++ project.clj
@@ -3,14 +3,14 @@
   :url "https://github.com/Wilfred/solo-wiki"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [compojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [compojure "1.6.0"]
                  [ring-anti-forgery "0.2.1"]
                  [org.clojure/java.jdbc "0.2.3"]
-                 [org.xerial/sqlite-jdbc "3.7.2"]
-                 [hiccup "1.0.2"]]
-  :plugins [[lein-ring "0.9.7"]
-            [lein-exec "0.3.6"]]
+                 [org.xerial/sqlite-jdbc "3.21.0.1"]
+                 [hiccup "1.0.5"]]
+  :plugins [[lein-ring "0.12.3"]
+            [lein-exec "0.3.7"]]
   :ring {:handler solo-wiki.handler/app}
   :min-lein-version "2.0.0"
   :profiles
